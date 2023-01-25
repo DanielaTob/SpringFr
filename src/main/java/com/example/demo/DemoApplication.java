f24c4f8a8d185adca9d7628359753b678f1ff77f
daniela <moreno.tobar.daniela@gmail.com>
2023-01-24 19:46:56 -0500

    application context

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.core.io.FileSystemResource;
 
 @SpringBootApplication
@@ -11,8 +13,14 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         //Triangle triangle = new Triangle();
-        BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-        Triangle triangle = (Triangle) factory.getBean("triangle");
+        //- EJEMPLO CON BEAN FACTORY -
+        //BeanFactory factory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+        //Triangle triangle = (Triangle) factory.getBean("triangle");
+        //triangle.draw();
+
+        //- EJEMPLO CON APLICATION CONTEXT -
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Triangle triangle = (Triangle) context.getBean("triangle");
         triangle.draw();
 
         SpringApplication.run(DemoApplication.class, args);
--- src/main/java/com/example/demo/Triangle.java
+++ src/main/java/com/example/demo/Triangle.java
@@ -2,7 +2,17 @@ package com.example.demo;
 
 public class Triangle {
 
+    public String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
     public void draw(){
-        System.out.println("Triangle draw");
+        System.out.println(getType() + " Triangle draw");
     }
 }
