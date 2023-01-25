2a97b8d818a47461e95fad2cd05a10b4d4d7d2b0
daniela <moreno.tobar.daniela@gmail.com>
2023-01-25 12:54:02 -0500

    constructor injection

--- src/main/java/com/example/demo/Triangle.java
+++ src/main/java/com/example/demo/Triangle.java
@@ -2,17 +2,37 @@ package com.example.demo;
 
 public class Triangle {
 
-    public String type;
+    private String type;
+    private int height;
+
+    //Constructor que se sobre pone sobre el otro
+    public Triangle(String type, int height){
+        this.type = type;
+        this.height = height;
+    }
+    public int getHeight() {
+        return height;
+    }
+
+    //Constructor
+    public Triangle(int height){
+        this.height = height;
+    }
+
+    //Constructor
+    public Triangle(String type){
+        this.type = type;
+    }
 
     public String getType() {
         return type;
     }
 
-    public void setType(String type) {
+    /*public void setType(String type) {
         this.type = type;
-    }
+    }*/
 
     public void draw(){
-        System.out.println(getType() + " Triangle draw");
+        System.out.println(getType() + " Triangle draw of height " + getHeight());
     }
 }
