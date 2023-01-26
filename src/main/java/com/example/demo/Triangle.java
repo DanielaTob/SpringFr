5497cb6d5e034ba89f3c17ab8106771b5d278061
daniela <moreno.tobar.daniela@gmail.com>
2023-01-26 18:24:32 -0500

    application context aware

--- src/main/java/com/example/demo/Triangle.java
+++ src/main/java/com/example/demo/Triangle.java
@@ -1,12 +1,18 @@
 package com.example.demo;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
 import java.util.List;
 
-public class Triangle {
+public class Triangle  implements ApplicationContextAware, BeanNameAware {
 
     private Point pointA;
     private Point pointB;
     private Point pointC;
+    private ApplicationContext applicationContext = null;
 
 
     public Point getPointA() {
@@ -37,5 +43,16 @@ public class Triangle {
         System.out.println("Point A = (" + getPointA().getX() + ", " + getPointA().getY() + ")");
         System.out.println("Point B = (" + getPointB().getX() + ", " + getPointB().getY() + ")");
         System.out.println("Point C = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext)
+            throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void setBeanName(String beanName) {
+        System.out.println("Bean name is: " + beanName);
     }
 }
