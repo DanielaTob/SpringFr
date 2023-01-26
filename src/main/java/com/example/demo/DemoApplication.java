72634a82409deee4b0ebf254ddec2fc714f3d33b
daniela <moreno.tobar.daniela@gmail.com>
2023-01-25 19:52:15 -0500

    inner beans

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -20,7 +20,7 @@ public class DemoApplication {
 
         //- EJEMPLO CON APLICATION CONTEXT -
         ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
-        Triangle triangle = (Triangle) context.getBean("triangle");
+        Triangle triangle = (Triangle) context.getBean("triangle-alias");
         triangle.draw();
 
         SpringApplication.run(DemoApplication.class, args);
